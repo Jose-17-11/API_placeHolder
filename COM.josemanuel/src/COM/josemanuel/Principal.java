f64e42a2804b21a175b30397dc3c5bc5d86d7ebb
Jose Manuel <chema8130@gmail.com>
2023-08-11 17:20:33 -0600

    Se extraen los datos en formato numerico ASCII

---
+++ COM.josemanuel/src/COM/josemanuel/Principal.java
@@ -0,0 +1,28 @@
+package COM.josemanuel;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.json.*;
+public class Principal {
+
+	public static void main(String[] args) throws Exception {
+//		Se instancia la clase url para ingresar a un URL
+		URL url = new URL("https://jsonplaceholder.typicode.com/users");
+//		Se llama la fucncion openConection para poder hacer una peticion externa
+		HttpURLConnection cx = (HttpURLConnection) url.openConnection();
+//		Se llama el metodo GET que extrae los datos de la URL 
+		cx.setRequestMethod("GET");
+		
+//		se crea el objeto strm que se le asignan todos los datos extraidos
+		InputStream strm = cx.getInputStream();
+//		se convierten todos los datos extraidos a un arreglo de tipo byte
+		byte[] arrStream = strm.readAllBytes();
+//		Se crea un for each para mostrar en pantalla todos los datos en formato numerico
+		for(byte tmp: arrStream) {
+			System.out.println(tmp);
+		}
+		
+	}
+
+}
