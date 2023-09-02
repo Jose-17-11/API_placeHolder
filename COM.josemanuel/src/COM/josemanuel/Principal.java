8f2edc1bd432542afd54b6c9ea1484e9188b3d93
Jose Manuel <chema8130@gmail.com>
2023-09-01 23:39:02 -0600

    Se ha ingresado un login exitosamente

--- COM.josemanuel/src/COM/josemanuel/Principal.java
+++ COM.josemanuel/src/COM/josemanuel/Principal.java
@@ -1,42 +1,57 @@
 package COM.josemanuel;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class Principal {
+	
+    public static void main(String[] args) throws Exception {
+    	String username = "Bret";
+        // Se instancia la clase URL para ingresar a un URL
+        URL url = new URL("https://jsonplaceholder.typicode.com/users");
+        // Se llama la función openConnection para poder hacer una petición externa
+        HttpURLConnection cx = (HttpURLConnection) url.openConnection();
+        // Se llama el método GET que extrae los datos de la URL
+        cx.setRequestMethod("GET");
 
-	public static void main(String[] args) throws Exception {
-//		Se instancia la clase url para ingresar a un URL
-		URL url = new URL("https://jsonplaceholder.typicode.com/users");
-//		Se llama la fucncion openConection para poder hacer una peticion externa
-		HttpURLConnection cx = (HttpURLConnection) url.openConnection();
-//		Se llama el metodo GET que extrae los datos de la URL 
-		cx.setRequestMethod("GET");
-		
-//		se crea el objeto strm que se le asignan todos los datos extraidos
-		InputStream strm = cx.getInputStream();
-//		se convierten todos los datos extraidos a un arreglo de tipo byte
-		byte[] arrStream = strm.readAllBytes();
-		
-//		Se creo una variable que almacenara los datos mas especificamente
-		String cntJson = "";
-		
-//		Se crea un for each para mostrar en pantalla todos los datos en formato numerico
-		for(byte tmp: arrStream) {
-//			System.out.println(tmp);
-/*			Aqui se hizo un cast a tipo char para que de esta forma pueda imprimir en consola exactamente igual a como se observa 
- * 			visualmente el original JSON
- *
- *			System.out.print((char)tmp);
-* **/			
-/*			La diferencia con el anterio es que realmente estaba imprimiendo caracter por caracter y en este caso no, en este caso se 
- * 			imprime todo dentro de una cadena de texto
- * */
-			cntJson += (char)tmp;
-			System.out.println(cntJson);
-		}
-		
-	}
+        // Se crea el objeto strm que se le asignan todos los datos extraídos
+        InputStream strm = cx.getInputStream();
+        // Se convierten todos los datos extraídos a un arreglo de tipo byte
+        byte[] arrStream = strm.readAllBytes();
 
+        // Se crea una variable que almacenará los datos más específicamente
+        String cntJson = "";
+
+        // Se crea un for each para mostrar en pantalla todos los datos en formato numérico
+        for (byte tmp : arrStream) {
+            cntJson += (char) tmp;
+        }
+
+        // Parsear el JSON y extraer los emails
+        JSONArray jsonArray = new JSONArray(cntJson);
+        //Cree la variable j y user inicializadas en cero y vacias
+        int j = 0;
+        String user = "";
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String obj = jsonObject.getString("username");
+            //La condicion if compara la cadena extraida del JSON y la compara con el usuario que hemos ingresado, si es el mismo hace lo siguiente
+            if(username.equals(obj)) {
+            	//Aunmenta un 1 en la variable j y la cadena user toma el valor del dato extraido del JSON
+            	j++;
+            	user = obj;
+            }
+            
+        }
+//        Esta condicion fuera del ciclo es la que nos deja acceder ya que si el contador aumento el usuario ingresado es correcto por lo tanto 
+//        puede iniciar sesion
+        if(j!=0) {
+        	System.out.println("Haz iniciado sesion");
+        	System.out.println("Usuario: " + user);
+        }else {
+        	System.out.println("Usuario no encontrado");
+        }
+    }
 }
